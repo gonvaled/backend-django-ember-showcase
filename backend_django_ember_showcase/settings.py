7a9641461ea2b05a214e2cfd357a09c43c20d050
Daniel Gonzalez (polyphemus) <gonvaled@gonvaled.com>
2016-01-06 19:55:05 +0100

    Parse database configuration from DATABASE_URL

--- backend_django_ember_showcase/settings.py
+++ backend_django_ember_showcase/settings.py
@@ -71,15 +71,9 @@ TEMPLATES = [
 WSGI_APPLICATION = 'backend_django_ember_showcase.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/1.9/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# Parse database configuration from $DATABASE_URL: https://github.com/kennethreitz/dj-database-url
+import dj_database_url
+DATABASES = { 'default': dj_database_url.config() }
 
 
 # Password validation
